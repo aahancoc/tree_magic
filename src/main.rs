e6bf482598213d375fd4f4694eb1520938b6d5a2
aahancoc <aahancoc@umflint.edu>
2017-03-27 03:13:12 -0400

    Error handling now more rusty

--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,25 @@ use std::io::prelude::*;
 use std::io::BufReader;
 use std::fs::File;
 
-fn main() {
-    let f = File::open("/usr/share/mime/types").ok().expect("mime/types file not found. Is this a *nix system?");
+fn load_types() -> Result<Vec<String>, std::io::Error> {
+    let f = File::open("/usr/share/mime/types")?;
     let reader = BufReader::new(f);
+    let mut out = Vec::new();
 
     for line in reader.lines() {
-        println!("{}", line.ok().expect("Could not read line"));
+        //println!("{}", line.ok().expect("Could not read line"));
+        out.push(line.ok().expect("Could not read line"));
+    }
+    
+    Ok(out)
+    
+}
+
+fn main() {
+    match load_types() {
+        Err(why) => panic!("{:?}", why),
+        Ok(ftypes) => for ftype in &ftypes {
+            println!("{}", ftype);
+        },
     }
 }
