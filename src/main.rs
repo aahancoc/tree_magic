bbb688acf1a3c177d8cd6a45eafc1f3d4e9acd56
aahancoc <aahancoc@umflint.edu>
2017-03-27 02:42:24 -0400

    Can read mine/types file

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+use std::io::prelude::*;
+use std::io::BufReader;
+use std::fs::File;
+
 fn main() {
-	println!("Hello, world!");
+    let f = File::open("/usr/share/mime/types").ok().expect("mime/types file not found. Is this a *nix system?");
+    let reader = BufReader::new(f);
+
+    for line in reader.lines() {
+        println!("{}", line.ok().expect("Could not read line"));
+    }
 }
