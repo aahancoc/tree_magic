1ac289f9b794f14b276ed90833829af51f0ab88e
aahancoc <aahancoc@umflint.edu>
2017-03-27 03:45:28 -0400

    Now reads subclasses file too

--- src/main.rs
+++ src/main.rs
@@ -8,8 +8,23 @@ fn load_types() -> Result<Vec<String>, std::io::Error> {
     let mut out = Vec::new();
 
     for line in reader.lines() {
-        //println!("{}", line.ok().expect("Could not read line"));
-        out.push(line.ok().expect("Could not read line"));
+        out.push(line?);
+    }
+    
+    Ok(out)
+    
+}
+
+fn load_relations() -> Result<Vec<(String, String)>, std::io::Error> {
+    let f = File::open("/usr/share/mime/subclasses")?;
+    let reader = BufReader::new(f);
+    let mut out = Vec::new();
+
+    for line in reader.lines() {
+        let line_raw = line?;
+        let parent = line_raw.split_whitespace().nth(0).unwrap_or("").to_string();
+        let child = line_raw.split_whitespace().nth(1).unwrap_or("").to_string();
+        out.push((parent, child));
     }
     
     Ok(out)
@@ -17,10 +32,18 @@ fn load_types() -> Result<Vec<String>, std::io::Error> {
 }
 
 fn main() {
-    match load_types() {
+
+    match load_relations() {
+        Err(why) => panic!("{:?}", why),
+        Ok(ftypes) => for ftype in &ftypes {
+            println!("{} -> {}", ftype.0, ftype.1);
+        },
+    };
+
+    /*match load_types() {
         Err(why) => panic!("{:?}", why),
         Ok(ftypes) => for ftype in &ftypes {
             println!("{}", ftype);
         },
-    }
+    };*/
 }
