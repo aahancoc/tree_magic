7606251e3056acc0e8569eb22e4cfef2f0e92623
aahancoc <aahancoc@umflint.edu>
2017-04-15 01:04:59 -0400

    Added parallelism to tmagic

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,16 @@
 //extern crate tree_magic;
 extern crate clap;
 extern crate tabwriter;
-extern crate crossbeam;
 extern crate tree_magic;
+extern crate walkdir;
+extern crate scoped_threadpool;
+extern crate num_cpus;
 
 use tabwriter::TabWriter;
 use std::io::prelude::*;
 use std::sync::mpsc;
-use std::sync::{Arc, Mutex};
+use walkdir::{WalkDir};
+use scoped_threadpool::Pool;
 
 #[cfg(not(feature="staticmime"))]
 macro_rules! convmime {
@@ -30,37 +33,77 @@ fn main() {
             .index(1)
             .multiple(true)
         )
+        .arg(Arg::with_name("recursive")
+            .short("r")
+            .help("Search directories recursively")
+        )
+        .arg(Arg::with_name("match")
+            .short("m")
+            .long("match")
+            .use_delimiter(true)
+            .takes_value(true)
+            .help("Print only files that match given MIME")
+        )
+        .arg(Arg::with_name("ugly")
+            .long("ugly")
+            .help("Print results as they come in, at expense of tab alignment")
+        )
         .get_matches();
-    let files: Vec<_> = args.values_of("file").unwrap().collect();
+    let mut files: Vec<String> = args.values_of("file")
+        .unwrap()
+        .map(|x| x.to_string())
+        .collect();
     
     let mut tw = TabWriter::new(vec![]);
     let (tx, rx) = mpsc::channel();
-    //let list = Arc::new(Mutex::new(Vec::<String>::new()));
     
-    crossbeam::scope(|scope| {
+    // Get recursive results if needed
+    if args.is_present("recursive") {
+        //println!("Recursive!");
+        for dir in files.clone() {
+            let entries = WalkDir::new(dir).into_iter().filter_map(|e| e.ok());
+            for entry in entries {
+                files.push(entry.path()
+                    .to_str()
+                    .unwrap()
+                    .to_string()
+                );
+            }
+        }
+        //println!("{:#?}", files);
+    }
+    
+    let mut pool = Pool::new(num_cpus::get() as u32);
+    let is_ugly = args.is_present("ugly");
+    
+    pool.scoped(|scope| {
         for x in files {
             let tx = tx.clone();
-            //let list = list.clone();
-            scope.spawn(move || {
-                let result = tree_magic::from_filepath(x).unwrap_or("inode/empty");
-                let result = format!("{}:\t{:?}\n", x, result);
-                //let ref mut list = *list.lock().unwrap();
-                //list.push(result);
-                tx.send(result).unwrap();
+            scope.execute(move || {
+                let result = tree_magic::from_filepath(x.as_str());
+                let result = result.unwrap_or(convmime!("inode/none"));
+                let result = format!("{}:\t{:?}", x, result);
+                if is_ugly {
+                    println!("{}", result);
+                } else {
+                    tx.send(result + "\n").unwrap_or_default();
+                }
             });
         }
     });
     drop(tx);
     
-    let mut list: Vec<String> = rx.iter().collect();
-    //let ref mut list = *list.lock().unwrap();
-    list.sort();
-    for x in list {
-        write!(&mut tw, "{}", x).unwrap();
+    if !is_ugly {
+        let mut list: Vec<_> = rx.iter().collect();
+        list.sort();
+        list.dedup();
+        for x in list {
+            write!(&mut tw, "{}", x).unwrap();
+        }
+        
+        tw.flush().unwrap();
+        let out = String::from_utf8(tw.into_inner().unwrap()).unwrap_or("".to_string());
+        println!("{}", out);
     }
-    
-    tw.flush().unwrap();
-    let out = String::from_utf8(tw.into_inner().unwrap()).unwrap_or("".to_string());
-    println!("{}", out);
     
 }
