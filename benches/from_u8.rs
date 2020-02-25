bdeac4272a8ec0b72954dbd372aa86e499648dd1
Allison Hancock <aahancoc@umich.edu>
2020-02-24 21:49:50 -0500

    Move from nightly bench to `bencher` crate

--- benches/from_u8.rs
+++ benches/from_u8.rs
@@ -1,29 +1,25 @@
-#![feature(test)]
-mod from_u8 {
-    extern crate test;
-    use self::test::Bencher;
-    extern crate tree_magic;
+#[macro_use]
+extern crate bencher;
+extern crate tree_magic;
+use bencher::Bencher;
 
-    ///Image tests
-    #[bench]
-    fn image_gif(b: &mut Bencher) {
-        b.iter(|| tree_magic::from_u8(include_bytes!("image/gif")));
-    }
-    #[bench]
-    fn image_png(b: &mut Bencher) {
-        b.iter(|| tree_magic::from_u8(include_bytes!("image/png")));
-    }
-    
-    /// Archive tests
-    #[bench]
-    fn application_zip(b: &mut Bencher) {
-        b.iter(|| tree_magic::from_u8(include_bytes!("application/zip")));
-    }
-
-    /// Text tests
-    #[bench]
-    fn text_plain(b: &mut Bencher) {
-        b.iter(|| tree_magic::from_u8(include_bytes!("text/plain")));
-    }
-
+///Image tests
+fn image_gif(b: &mut Bencher) {
+    b.iter(|| tree_magic::from_u8(include_bytes!("image/gif")));
 }
+fn image_png(b: &mut Bencher) {
+    b.iter(|| tree_magic::from_u8(include_bytes!("image/png")));
+}
+
+/// Archive tests
+fn application_zip(b: &mut Bencher) {
+    b.iter(|| tree_magic::from_u8(include_bytes!("application/zip")));
+}
+
+/// Text tests
+fn text_plain(b: &mut Bencher) {
+    b.iter(|| tree_magic::from_u8(include_bytes!("text/plain")));
+}
+
+benchmark_group!(benches, image_gif, image_png, application_zip, text_plain);
+benchmark_main!(benches);
--- benches/match_u8.rs
+++ benches/match_u8.rs
@@ -1,38 +1,25 @@
-#![feature(test)]
-mod match_u8 {
-    extern crate test;
-    use self::test::Bencher;
-    extern crate tree_magic;
+#[macro_use]
+extern crate bencher;
+extern crate tree_magic;
+use bencher::Bencher;
 
-    #[cfg(not(feature="staticmime"))]
-    macro_rules! convmime {
-        ($x:expr) => {$x.to_string()}
-    }
-    #[cfg(feature="staticmime")]
-    macro_rules! convmime {
-        ($x:expr) => {$x}
-    }
-
-    ///Image benchmarks
-    #[bench]
-    fn image_gif(b: &mut Bencher) {
-        b.iter(|| tree_magic::match_u8("image/gif", include_bytes!("image/gif")));
-    }
-    #[bench]
-    fn image_png(b: &mut Bencher) {
-        b.iter(|| tree_magic::match_u8("image/png", include_bytes!("image/png")));
-    }
-
-    /// Archive tests
-    #[bench]
-    fn application_zip(b: &mut Bencher) {
-        b.iter(|| tree_magic::match_u8("application/zip", include_bytes!("application/zip")));
-    }
-
-    /// Text tests
-    #[bench]
-    fn text_plain(b: &mut Bencher) {
-        b.iter(|| tree_magic::match_u8("text/plain", include_bytes!("text/plain")));
-    }
-
+///Image benchmarks
+fn image_gif(b: &mut Bencher) {
+    b.iter(|| tree_magic::match_u8("image/gif", include_bytes!("image/gif")));
 }
+fn image_png(b: &mut Bencher) {
+    b.iter(|| tree_magic::match_u8("image/png", include_bytes!("image/png")));
+}
+
+/// Archive tests
+fn application_zip(b: &mut Bencher) {
+    b.iter(|| tree_magic::match_u8("application/zip", include_bytes!("application/zip")));
+}
+
+/// Text tests
+fn text_plain(b: &mut Bencher) {
+    b.iter(|| tree_magic::match_u8("text/plain", include_bytes!("text/plain")));
+}
+
+benchmark_group!(benches, image_gif, image_png, application_zip, text_plain);
+benchmark_main!(benches);
